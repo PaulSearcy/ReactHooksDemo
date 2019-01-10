a0df19b38500751bccc392955b17ebb3d4fc1705
tridon777 <tridon777@gmail.com>
2019-01-09 19:49:27 -0500

    Webpack HotModule Removal  - removing react-hot-loader lib  as it was not removing itself in production mode and was outputting console message in browser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react'
-import {hot} from 'react-hot-loader'
 import {toHTML} from './utilities.js'
 
 let App = () => {
@@ -51,4 +50,4 @@ let App = () => {
         </div>
     )
 }
-export default hot(module)(App)
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -33,12 +33,5 @@ module.exports = {
         path: path.resolve(__dirname, "dist/"),
         publicPath: "/dist/",
         filename: "bundle.js"
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "public/"),
-        port: 3000,
-        publicPath: "http://localhost:3000/dist/",
-        hotOnly: true
-    },
-    plugins: [new webpack.HotModuleReplacementPlugin()]
+    }
 };
